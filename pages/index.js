afc649b9878f164628c6dd8312dc243244898f7d
wilfried fonkeng <afk1995wilfried@gmail.com>
2020-08-24 20:10:44 +0100

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,112 @@
-import Head from 'next/head'
+
 import styles from '../styles/Home.module.css'
 
+import Head from 'next/head'
+//import {Animated} from "react-animated-css";
+//import {Navbar, Nav, NavDropdown, Form, FormControl, 
+//} from 'react-bootstrap';
+import { Row, Col, Card, Container, Button, img,Carousel } from 'react-bootstrap';
+import Header from  './components/shared/header'
+import Footer from  './components/shared/footer'
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div>
+       <Head>
+      <title>Macgregor marine service</title>
+      <link rel="icon" href="/favicon.PNG" />
+      
+    </Head>
+      
+      <Header/>
+    
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+      <div className="container mt-5">
+      <Row>
+    <Col sm={6}>
+    <div className="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+        <div className="col p-4 d-flex flex-column position-static">
+    <h2 className="mb-2">MacGregor Marine Services</h2>
+    <p className="card-text mb-auto justify">
+    Est une entreprise spécialisée dans les services maritimes et l’affrètement dans le golfe de guinée.
+Constituée d’une équipe de professionnel à votre écoute. Nous vous offrons une solution adaptée et sur
+mesure pour chaque type d’opération.
+      </p>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+
+
+
+  
+   <p className="card-text mb-auto justify">
+    Est une entreprise spécialisée dans les services maritimes et l’affrètement dans le golfe de guinée.
+Constituée d’une équipe de professionnel à votre écoute. Nous vous offrons une solution adaptée et sur
+mesure pour chaque type d’opération.
+      </p>
+
+      </div>
+      </div>
+    </Col>
+    <Col sm={6}>
+    <Card style={{ width: '20rem' }}>
+  <Card.Img variant="top" src="Damen.jpg" />
+  </Card>
+
+       
+      
+    </Col>
+    <br>
+    </br>
+  </Row>
+
+
+
+  <Row className= "mt-5">
+    <Col sm={6}>
+    <div className="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+        <div className="col p-4 d-flex flex-column position-static">
+    <h2 className="mb-2">Zone d'activité </h2>
+    <p className="card-text mb-auto justify">
+    Afin de répondre aux contraintes de lieu, nous vous proposons des solutions à chaque projet, à travers des
+            études de faisabilité logistique et des plans d’exécution.
+      </p>
+      </div>
+      </div>
+    </Col>
+    <Col sm={6}>
+    
+     <Card style={{ width: '20rem' }}>
+  <Card.Img variant="top" src="zone d'activité.PNG" />
+  </Card>
+
+    
+
+       </Col>
+    <br>
+    </br>
+  </Row>
+
+ 
+
+
+      </div>
+<Footer />
+
+
+      
+
+      <style jsx>{
+        `.mb-2{
+          color: #002e5d;
+          
+        }
+        `
+      }
+
+      </style>
+</div>
   )
 }
---
+++ pages/servicelist.js
@@ -0,0 +1,76 @@
+import { Row, Col, Card, Container, Image } from 'react-bootstrap';
+//import {faMapMarker, faPhone } from '@fortawesome/free-solid-svg-icons';
+export default function Servicelist() {
+    return(
+        <div>
+<Container>        
+<Row className="mt-5">
+<Col md={6}>
+    <div className="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+        <div className="col p-4 d-flex flex-column position-static">
+    <h2 className="mb-2">MacGregor Marine Services</h2>
+    <ul>
+        <li>
+       <strong> Transport fluviomaritime dans le golfe de Guinée</strong> </li>
+ 
+ <li><strong>Mise à disposition d’attelage : Remorqueur + barge</strong> </li>
+  
+ <li><strong>Remorquage et assistance</strong></li> 
+  
+ <li><strong>Transbordement</strong>  </li>
+    </ul>
+    
+      </div>
+      </div>
+</Col>
+
+
+<Col md={6}>
+<Card style={{ width: '20rem' }}>
+  <Card.Img variant="top" src="barq.jpg" />
+  </Card>
+</Col>
+
+
+      </Row>
+  <Row>
+  <Col xs={12} md={12}>
+  <div className="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+         <div className="col p-4 d-flex flex-column position-static">
+
+        <Image src="location.jpg" className="text-center" roundedCircle  style={{ width: '18rem' }}/>
+        <h4 className="text-center"> Nous intervenons sur le golfe de Guinée notamment au:</h4>
+    <ul >
+        <li>
+       <strong>Nigéria</strong> </li>
+ 
+ <li><strong>Cameroun</strong> </li>
+  
+ <li><strong>Guinée équatoriale</strong></li> 
+  
+ <li><strong>Gabon</strong>  </li>
+ <li><strong>Congo</strong>  </li>
+ <li><strong>Angola</strong>  </li>
+ <li><strong>Sao Tomé et principe</strong>  </li>
+    </ul>
+    
+      </div>
+      </div>
+      </Col>
+  </Row>
+
+   
+
+      </Container>
+      <style jsx>{
+        `.mb-2{
+          color: #002e5d;
+          
+        }
+        `
+      }
+
+      </style>
+        </div>
+    )
+}
